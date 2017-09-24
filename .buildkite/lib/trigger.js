c6ed67dfbb454517dc1ede03f53d9c908371a8ad
Zac Lang <zac.wr.lang@gmail.com>
2017-09-24 11:42:53 +1000

    allow services to define their own build & deploy steps

--- .buildkite/lib/trigger.js
+++ .buildkite/lib/trigger.js
@@ -13,20 +13,18 @@ const {
   BUILDKITE_COMMIT,
 } = process.env;
 
-
 generateDynamicPipeline(BUILDKITE_COMMIT, CHANGES_LIST_FILENAME, TEMPLATE_OUTPUT_FILENAME);
 
 async function generateDynamicPipeline(commit, changesFilename, outputFilename) {
   try {
-    const fileContents = await readFileAsync(changesFilename, {encoding: 'utf8'});
-    const changedServices = JSON.parse(fileContents);
+    const changedServices = await parseFile(changesFilename);
 
     if (!changedServices.length) {
       console.log('no changed services!');
       return;
     }
 
-    const generatedTemplate = buildPipelineSteps(commit, changedServices);
+    const generatedTemplate = await buildPipelineSteps(commit, changedServices);
 
     await writeFileAsync(outputFilename, JSON.stringify(generatedTemplate, null, 2));
 
@@ -38,21 +36,32 @@ async function generateDynamicPipeline(commit, changesFilename, outputFilename) 
   }
 }
 
-function buildPipelineSteps(commit, services = []) {
+async function readStepsForChangedServices(services) {
+  // todo: refactor this!
+  const build = await Promise.all(services.map(service => parseFile(`${service}/.buildkite/build.json`)));    
+  const deployStaging = await Promise.all(services.map(service => parseFile(`${service}/.buildkite/deploy-staging.json`)));
+  const deployProd = await Promise.all(services.map(service => parseFile(`${service}/.buildkite/deploy-prod.json`)));
+  return { build, deployStaging, deployProd };
+}
+
+async function buildPipelineSteps(commit, services = []) {
   const DEFAULT_TEMPLATE = {env: {}, steps: []};
 
-  const steps = services.map(service => ({
-    trigger: service,
-    name: `Building ${service} :node::docker:`,
-    async: false,
-    build: { commit }
-  }));
+const { build, deployStaging, deployProd } = await readStepsForChangedServices(services);
 
-  if (steps.length) {
-    Array.prototype.push.apply(steps, [
-      {
-        type: "waiter",
-      },
+  if (!services.length) { return DEFAULT_TEMPLATE };
+
+  const steps = build.reduce((acc, item) => acc.concat(item.steps), [])
+  
+    // wait for builds to complete
+
+    .concat([{ type: "waiter" }])
+    .concat(deployStaging.reduce((acc, item) => acc.concat(item.steps), []))
+
+    // wait for staging deployments to complete
+
+    .concat([    
+      { type: "waiter" },
       {
         "type": "script",
         "name": "e2e-staging :pray:",
@@ -61,11 +70,10 @@ function buildPipelineSteps(commit, services = []) {
       {
         block: "Release :red_button: :dragon:",
       }]
-      .concat(services.map(service => ({
-        "type": "script",
-        "name": `Deploying ${service}`,
-        "command": `echo 'deploying ${service}'`,
-      }))
+      .concat(deployProd.reduce((acc, item) => acc.concat(item.steps), []))
+
+      // wait for prod deployments to complete
+
       .concat([
         {
           type: "waiter",
@@ -75,14 +83,22 @@ function buildPipelineSteps(commit, services = []) {
           "name": "e2e-prod :pray:",
           "command": "echo 'e2e'",
         }
-      ])
-    ));
-  }
+      ])    
+    );  
 
   return Object.assign(
     DEFAULT_TEMPLATE,
-    { steps }   
+    { steps }, 
   );
+}
+
+async function parseFile(path) {
+  try {
+  const contents = await readFileAsync(path, { encoding: 'utf8' });   
+  return JSON.parse(contents);
+  } catch (error) {
+    return '';
+  }
 }
 
 function uploadPipelineToBuildkite(filePath) {
